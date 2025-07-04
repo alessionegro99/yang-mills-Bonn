90b5d9f4910ef3917ee5ffd2b700faf6f19b4f7d
alessionegro99 <alessio.negro1999@gmail.com>
2025-07-04 17:12:20 +0200

    updates

--- src/yang_mills_local_obc.c
+++ src/yang_mills_local_obc.c
@@ -60,7 +60,7 @@ void real_main(char *in_file)
     time(&time1);
 
     // check on the starting conf
-    perform_measures_localobs_obc(&GC, &geo, &param, datafilep);
+    perform_measures_localobs_obc(&GC, &geo, &param, datafilep, datafileW, datafilesW);
     // count starts from 1 to avoid problems using %
     for(count=1; count < param.d_sample + 1; count++)
        {        
@@ -68,7 +68,7 @@ void real_main(char *in_file)
 
         if(count % param.d_measevery ==0 && count >= param.d_thermal)
           {
-          perform_measures_localobs_obc(&GC, &geo, &param, datafilep);
+          perform_measures_localobs_obc(&GC, &geo, &param, datafilep, datafileW, datafilesW);
           }
 
        // save configuration for backup
@@ -158,6 +158,8 @@ void print_template_input(void)
     fprintf(fp, "#output files\n");
     fprintf(fp, "conf_file  conf.dat\n");
     fprintf(fp, "data_file  dati.dat\n");
+    fprintf(fp, "Wloop_file  Wloop.dat\n");
+    fprintf(fp, "sWloop_file  sWloop.dat\n");
     fprintf(fp, "mon_file   mon.dat\n");
     fprintf(fp, "log_file   log.dat\n");
     fprintf(fp, "\n");
